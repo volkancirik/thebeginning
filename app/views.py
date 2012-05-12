5d968b685f24736e925d769067a1e880d5ecca1a
Volkan <volkan.cirik@gmail.com>
2012-05-12 12:02:55 +0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,4 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+def synopsis(request):
+   return render_to_response("synopsis.html")
---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import patterns, include, url
+from thebeginning.settings import MEDIA_ROOT
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'thebeginning.views.home', name='home'),
+    # url(r'^thebeginning/', include('thebeginning.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^img/(?P<path>.*)$', 'django.views.static.serve',{'document_root': MEDIA_ROOT}),
+    (r'^synopsis/$', 'thebeginning.app.views.synopsis'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'thebeginning.app.views.synopsis'), # Should be the last element.
+)
